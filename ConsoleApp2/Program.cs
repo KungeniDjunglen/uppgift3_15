19b14df598e15a3f338ed92cf6155dfae6b9cf76
AlemHalilovic <alha05001@utb.vaxjo.se>
2022-09-30 10:09:43 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammla är du?");
+            int age = int.Parse(Console.ReadLine());
+            if (age <= 16)
+            {
+                Console.WriteLine("Du är för ung");
+            }
+            else if (age >= 16 & age <= 20)
+            {
+                Console.WriteLine("Du är i rätt åldersgrupp");
+            }
+            else
+            {
+                Console.WriteLine("Du är för gammal");
+            }
+            
+        }
+    }
+}
